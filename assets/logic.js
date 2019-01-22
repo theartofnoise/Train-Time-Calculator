36eb4c801ee5c4f3a0865b81cd68a1498f8060db
Art G <theartofnoise@gmail.com>
2019-01-22 14:52:54 -0500

    think i basically have the time conversion done

--- assets/logic.js
+++ assets/logic.js
@@ -1,6 +1,6 @@
 $(document).ready(function (){
     function setTime() {
-    var now = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+    var now = moment().format("dddd, MMMM Do YYYY, HH:mm:ss");
     $("#timeNow").text(now);
 };
 setInterval(setTime, 1000);
@@ -47,31 +47,44 @@ setInterval(setTime, 1000);
   });
 
   //Time conversion
-  function timeConversion(startT, freqT) {
-      var now = moment().format("LT")
+//   function timeConversion(startT, freqT) {
+//       var now = moment().format("LT")
+//       console.log(now);
+//       var lastYearStartT = moment(startT,"LT").subtract(1, "years").format("LT");
+//       console.log("start last year: "+lastYearStartT);
+//       console.log("freqT: "+ freqT);
+//       var diffT = moment().subtract(startT, "LT").format("m");
+//       console.log("diff: " + diffT);
+//       var r = diffT % freqT;
+//       console.log("r: "+r);
+//       var minsAway = freqT - r;
+//       console.log("mins away: "+minsAway);
+//       var arriveT = moment().add(minsAway, "m").format("LT");
+//       console.log("arriveT: "+arriveT);
+      
+//   };
+  
+  db.on("child_added", function(childSnap, arriveT, minsAway) {
+        //time conversion
+        var startT = childSnap.val().FirstTrainTime;
+        var freqT = childSnap.val().frequency;
+        var now = moment().format("HH:mm")
       console.log(now);
-      var lastYearStartT = moment(startT,"LT").subtract(1, "years").format("LT");
+      var lastYearStartT = moment(startT,"HH:mm").subtract(1, "years").format("HH:mm");
       console.log("start last year: "+lastYearStartT);
       console.log("freqT: "+ freqT);
-      var diffT = moment().subtract(startT, "LT").format("m");
+      var diffT = moment().subtract(startT, "HH:mm").format("m");
       console.log("diff: " + diffT);
       var r = diffT % freqT;
       console.log("r: "+r);
       var minsAway = freqT - r;
       console.log("mins away: "+minsAway);
-      var arriveT = moment().add(minsAway, "m").format("LT");
+      var arriveT = moment().add(minsAway, "m").format("HH:mm");
       console.log("arriveT: "+arriveT);
       
-  };
-  
-  db.on("child_added", function(childSnap, arriveT, minsAway) {
-      
-      var newRow = "<tr><td>" + childSnap.val().name + "</td><td>" + childSnap.val().destination + "</td><td>" + childSnap.val().frequency + "</td><td>" + arriveT + "</td><td>" + minsAway + "</td></tr>"
-    //   console.log(childSnap.val().FirstTrainTime);
-      var startT = childSnap.val().FirstTrainTime;
-      var freqT = childSnap.val().frequency;
+      var newRow = "<tr><td>" + childSnap.val().name + "</td><td>" + childSnap.val().destination + "</td><td>" + childSnap.val().frequency + "</td><td>" + minsAway + "</td><td>" + arriveT + "</td></tr>"
     //   console.log(freqT);
-      timeConversion(startT, freqT);
+    //   timeConversion(startT, freqT);
       $('#topRow').append(newRow);
       
     }, function(errorObject) {
